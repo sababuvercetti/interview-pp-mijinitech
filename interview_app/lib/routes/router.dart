f5fdb0f75d131b1ec8ef13092ca01fd79ca7ab45
Samuel Baraka <sababuvercetti@gmail.com>
2022-04-20 14:09:03 +0300

    Added new routes

--- interview_app/lib/routes/router.dart
+++ interview_app/lib/routes/router.dart
@@ -1,7 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:interview_app/UI/auth/login_page.dart';
+import 'package:interview_app/UI/auth/new_password_page.dart';
 import 'package:interview_app/UI/auth/reset_password_page.dart';
 import 'package:interview_app/UI/auth/signup_page.dart';
+import 'package:interview_app/UI/home/home_page.dart';
 
 @MaterialAutoRouter(
   replaceInRouteName: 'Page,Route',
@@ -9,6 +11,7 @@ import 'package:interview_app/UI/auth/signup_page.dart';
     AutoRoute(page: LoginPage, initial: true),
     AutoRoute(page: SignUpPage),
     AutoRoute(page: ResetPasswordPage),
+    AutoRoute(page: HomePage),
   ],
 )
 class $AppRouter {}
--- interview_app/lib/routes/router.gr.dart
+++ interview_app/lib/routes/router.gr.dart
@@ -10,44 +10,54 @@
 //
 // ignore_for_file: type=lint
 
-import 'package:auto_route/auto_route.dart' as _i4;
-import 'package:flutter/material.dart' as _i5;
+import 'package:auto_route/auto_route.dart' as _i5;
+import 'package:firebase_auth/firebase_auth.dart' as _i7;
+import 'package:flutter/material.dart' as _i6;
 
 import '../UI/auth/login_page.dart' as _i1;
 import '../UI/auth/reset_password_page.dart' as _i3;
 import '../UI/auth/signup_page.dart' as _i2;
+import '../UI/home/home_page.dart' as _i4;
 
-class AppRouter extends _i4.RootStackRouter {
-  AppRouter([_i5.GlobalKey<_i5.NavigatorState>? navigatorKey])
+class AppRouter extends _i5.RootStackRouter {
+  AppRouter([_i6.GlobalKey<_i6.NavigatorState>? navigatorKey])
       : super(navigatorKey);
 
   @override
-  final Map<String, _i4.PageFactory> pagesMap = {
+  final Map<String, _i5.PageFactory> pagesMap = {
     LoginRoute.name: (routeData) {
-      return _i4.MaterialPageX<dynamic>(
+      return _i5.MaterialPageX<dynamic>(
           routeData: routeData, child: _i1.LoginPage());
     },
     SignUpRoute.name: (routeData) {
-      return _i4.MaterialPageX<dynamic>(
+      return _i5.MaterialPageX<dynamic>(
           routeData: routeData, child: _i2.SignUpPage());
     },
     ResetPasswordRoute.name: (routeData) {
-      return _i4.MaterialPageX<dynamic>(
+      return _i5.MaterialPageX<dynamic>(
           routeData: routeData, child: _i3.ResetPasswordPage());
+    },
+    HomeRoute.name: (routeData) {
+      final args = routeData.argsAs<HomeRouteArgs>();
+      return _i5.MaterialPageX<dynamic>(
+          routeData: routeData,
+          child:
+              _i4.HomePage(key: args.key, userCredential: args.userCredential));
     }
   };
 
   @override
-  List<_i4.RouteConfig> get routes => [
-        _i4.RouteConfig(LoginRoute.name, path: '/'),
-        _i4.RouteConfig(SignUpRoute.name, path: '/sign-up-page'),
-        _i4.RouteConfig(ResetPasswordRoute.name, path: '/reset-password-page')
+  List<_i5.RouteConfig> get routes => [
+        _i5.RouteConfig(LoginRoute.name, path: '/'),
+        _i5.RouteConfig(SignUpRoute.name, path: '/sign-up-page'),
+        _i5.RouteConfig(ResetPasswordRoute.name, path: '/reset-password-page'),
+        _i5.RouteConfig(HomeRoute.name, path: '/home-page')
       ];
 }
 
 /// generated route for
 /// [_i1.LoginPage]
-class LoginRoute extends _i4.PageRouteInfo<void> {
+class LoginRoute extends _i5.PageRouteInfo<void> {
   const LoginRoute() : super(LoginRoute.name, path: '/');
 
   static const String name = 'LoginRoute';
@@ -55,7 +65,7 @@ class LoginRoute extends _i4.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i2.SignUpPage]
-class SignUpRoute extends _i4.PageRouteInfo<void> {
+class SignUpRoute extends _i5.PageRouteInfo<void> {
   const SignUpRoute() : super(SignUpRoute.name, path: '/sign-up-page');
 
   static const String name = 'SignUpRoute';
@@ -63,9 +73,33 @@ class SignUpRoute extends _i4.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i3.ResetPasswordPage]
-class ResetPasswordRoute extends _i4.PageRouteInfo<void> {
+class ResetPasswordRoute extends _i5.PageRouteInfo<void> {
   const ResetPasswordRoute()
       : super(ResetPasswordRoute.name, path: '/reset-password-page');
 
   static const String name = 'ResetPasswordRoute';
+}
+
+/// generated route for
+/// [_i4.HomePage]
+class HomeRoute extends _i5.PageRouteInfo<HomeRouteArgs> {
+  HomeRoute({_i6.Key? key, required _i7.UserCredential userCredential})
+      : super(HomeRoute.name,
+            path: '/home-page',
+            args: HomeRouteArgs(key: key, userCredential: userCredential));
+
+  static const String name = 'HomeRoute';
+}
+
+class HomeRouteArgs {
+  const HomeRouteArgs({this.key, required this.userCredential});
+
+  final _i6.Key? key;
+
+  final _i7.UserCredential userCredential;
+
+  @override
+  String toString() {
+    return 'HomeRouteArgs{key: $key, userCredential: $userCredential}';
+  }
 }
