a307ea396799ff8fb80f1c21d34ddf61bddf20a0
Samuel Baraka <sababuvercetti@gmail.com>
2022-04-20 13:20:55 +0300

    Authentication pages design

--- interview_app/lib/UI/auth/login_page.dart
+++ interview_app/lib/UI/auth/login_page.dart
@@ -1,26 +1,49 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:interview_app/providers/auth_provider.dart';
+import 'package:interview_app/routes/router.gr.dart';
+import 'package:provider/provider.dart';
+import 'package:interview_app/providers/global_providers.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class LoginPage extends ConsumerWidget {
+  void updateEmail(BuildContext context, String email) {
+    context.read<EmailProvider>().save(email);
+  }
 
+  void updatePassword(BuildContext context, String pass) {
+    context.read<PasswordProvider>().save(pass);
+  }
+
+  static var formKey = GlobalKey<FormBuilderState>();
   @override
-  Widget build(BuildContext context) {
-    var formKey = GlobalKey<FormBuilderState>();
+  Widget build(BuildContext context, ref) {
+    // String email = ref.read(emailProvider).state;
+    // String pass = ref.read(passwordProvider).state;
+    final _auth = ref.read(authUtilProvider);
+
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
+        padding: const EdgeInsets.all(20.0),
         child: FormBuilder(
           key: formKey,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              const Text(
+                'Login',
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
               const FlutterLogo(size: 200),
               FormBuilderTextField(
                 name: 'email',
+                onChanged: (value) => updateEmail(context, value!),
                 decoration: const InputDecoration(labelText: 'Email'),
                 validator: FormBuilderValidators.compose([
                   FormBuilderValidators.required(),
@@ -32,6 +55,7 @@ class LoginPage extends StatelessWidget {
               ),
               FormBuilderTextField(
                 name: 'password',
+                onChanged: (value) => updatePassword(context, value!),
                 decoration: const InputDecoration(labelText: 'Password'),
                 validator: FormBuilderValidators.compose([
                   FormBuilderValidators.required(),
@@ -41,13 +65,68 @@ class LoginPage extends StatelessWidget {
                 height: 20,
               ),
               CupertinoButton(
-                  child: const Text('Login'),
-                  color: Colors.blue,
-                  onPressed: () {
-                    if (formKey.currentState!.saveAndValidate()) {
-                      print(formKey.currentState!.value);
-                    }
-                  }),
+                onPressed: () async {
+                  if (formKey.currentState!.saveAndValidate()) {
+                    await _auth
+                        .signIn(
+                            email: formKey.currentState!.value['email'],
+                            password: formKey.currentState!.value['password'])
+                        .catchError((error) {
+                      showCupertinoDialog(
+                          context: context,
+                          builder: (context) => CupertinoAlertDialog(
+                                content: Text(error.toString()),
+                              ));
+                    });
+                  }
+                },
+                color: Colors.blue,
+                child: const Text('Login'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text.rich(TextSpan(
+                text: 'Don\'t have an account? ',
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+                children: [
+                  TextSpan(
+                    text: 'Sign up',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.blue,
+                    ),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        AutoRouter.of(context).push(SignUpRoute());
+                      },
+                  ),
+                ],
+              )),
+              const SizedBox(
+                height: 20,
+              ),
+              Text.rich(TextSpan(
+                text: 'Forgot your password ? ',
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+                children: [
+                  TextSpan(
+                    text: 'Reset password',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.blue,
+                    ),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        AutoRouter.of(context).push(ResetPasswordRoute());
+                      },
+                  ),
+                ],
+              )),
             ],
           ),
         ),
--- interview_app/lib/UI/auth/signup_page.dart
+++ interview_app/lib/UI/auth/signup_page.dart
@@ -1,12 +1,89 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:interview_app/providers/auth_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:interview_app/providers/global_providers.dart';
 
-class SignUpPage extends StatelessWidget {
-  const SignUpPage({ Key? key }) : super(key: key);
+class SignUpPage extends ConsumerWidget {
+  void updateEmail(BuildContext context, String email) {
+    context.read<EmailProvider>().save(email);
+  }
 
+  void updatePassword(BuildContext context, String pass) {
+    context.read<PasswordProvider>().save(pass);
+  }
+
+  static var formKey = GlobalKey<FormBuilderState>();
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      
+  Widget build(BuildContext context, ref) {
+    // String email = ref.read(emailProvider).state;
+    // String pass = ref.read(passwordProvider).state;
+    final _auth = ref.read(authUtilProvider);
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: FormBuilder(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Sign up',
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+              const FlutterLogo(size: 200),
+              FormBuilderTextField(
+                name: 'email',
+                onChanged: (value) => updateEmail(context, value!),
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.required(),
+                  FormBuilderValidators.email(),
+                ]),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              FormBuilderTextField(
+                name: 'password',
+                onChanged: (value) => updatePassword(context, value!),
+                decoration: const InputDecoration(labelText: 'Password'),
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.required(),
+                ]),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              CupertinoButton(
+                onPressed: () async {
+                  if (formKey.currentState!.saveAndValidate()) {
+                    await _auth
+                        .signUp(
+                            email: formKey.currentState!.value['email'],
+                            password: formKey.currentState!.value['password'])
+                        .catchError((error) {
+                      showCupertinoDialog(
+                          context: context,
+                          builder: (context) => CupertinoAlertDialog(
+                                content: Text(error.toString()),
+                              ));
+                    });
+                  }
+                },
+                color: Colors.blue,
+                child: const Text('Sign up'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
