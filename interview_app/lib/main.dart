170bb6172f49fff998e9279094e7f01e6083b0ed
Samuel Baraka <sababuvercetti@gmail.com>
2022-04-20 18:30:04 +0300

    providers reinstates

--- interview_app/lib/main.dart
+++ interview_app/lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:interview_app/firebase_options.dart';
 import 'package:interview_app/providers/global_providers.dart';
 import 'package:interview_app/routes/router.gr.dart';
-// import 'package:provider/provider.dart' as s;
+import 'package:provider/provider.dart' as s;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,20 +22,36 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Flutter Interview APP',
-      routerDelegate: _appRouter.delegate(),
-      routeInformationParser: _appRouter.defaultRouteParser(),
-      supportedLocales: const [
-        Locale('en'),
+    return s.MultiProvider(
+      providers: [
+        s.Provider<EmailProvider>(
+          create: (_) => EmailProvider(),
+        ),
+        s.Provider<PasswordProvider>(
+          create: (_) => PasswordProvider(),
+        ),
+        s.Provider<CodeProvider>(
+          create: (_) => CodeProvider(),
+        ),
+        s.Provider<HomeBottomIndexProvider>(
+          create: (_) => HomeBottomIndexProvider(),
+        ),
       ],
-      localizationsDelegates: const [
-        FormBuilderLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      child: MaterialApp.router(
+        title: 'Flutter Interview APP',
+        routerDelegate: _appRouter.delegate(),
+        routeInformationParser: _appRouter.defaultRouteParser(),
+        supportedLocales: const [
+          Locale('en'),
+        ],
+        localizationsDelegates: const [
+          FormBuilderLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
       ),
     );
   }
